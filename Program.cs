104f6c4f96848a0637447f3f1ce5cbb4cbebd848
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-07-12 11:57:24 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter file path:");
+        string inputPath = Console.ReadLine();
+
+        if (!File.Exists(inputPath))
+        {
+            Console.WriteLine("I can`t find file!");
+            return;
+        }
+
+        string content = File.ReadAllText(inputPath);
+        char[] rever = content.ToCharArray();
+        Array.Reverse(rever);
+
+        string reversedPath = "reversed.txt";
+        File.WriteAllText(reversedPath, new string(rever));
+
+    }
+}
